daca77f3f489805cdb42354fe27fddc399cd53ae
ikeda46 <shiro@ism.ac.jp>
2018-05-21 07:21:54 +0900

    Delete costF_nufft_sqTV.m

--- matlab/lib/costF_nufft_sqTV.m
+++
@@ -1,7 +0,0 @@
-function f = costF_nufft_sqTV(yax,x,Nx,Ny,lambda2)
-% function f = costF_nufft_sqTV(yAx,x,Nx,Ny,lambda,lambda2);
-
-f = real(yax'*yax)/2+lambda2*sqTV(x,Nx,Ny);
-%fprintf('costs: %g %g\n',real(yax'*yax)/2,lambda2*sqTV(x,Nx,Ny));
-
-end
